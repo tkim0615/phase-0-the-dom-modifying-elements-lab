883bbc21f78e8f93ebf62fda747eac96a16980b0
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-15 13:33:27 -0500

    completed lab with new solutions

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-// Write your code here!
-main.remove();
-const newHeader = document.createElement("h1")
-newHeader.id = 'victory';
-newHeader.textContent = "Tyler is the champion";
+//1
+const main = document.getElementById('main')
+main.remove()
+console.log(main)
+
+//2
+const newHeader = document.createElement('h1')
+console.log(newHeader)
+
+//3
+
+newHeader.id = "victory"
+newHeader.textContent = "Tyler is the champion"
+
+console.log(newHeader)
